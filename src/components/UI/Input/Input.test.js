c71e93e6f4f112b8c1f63f9f0854c5550c8d8766
Jefri Martinez <jefrijmartinez@gmail.com>
2022-01-27 01:35:04 -0400

    Updating Input

--- src/components/UI/Input/Input.test.js
+++ src/components/UI/Input/Input.test.js
@@ -1,28 +1,34 @@
 import Input from "../Input/Input";
 import { shallow } from "enzyme";
 
+const setup = (props = {}) => {
+  const propsPassed = { ...props };
+  const wrapper = shallow(<Input {...propsPassed} />);
+  return wrapper;
+};
+
 describe("Input", () => {
   it("should render without errors", () => {
-    const component = shallow(<Input />);
+    const component = setup();
     const wrapper = component.find(".input");
     expect(wrapper.length).toBe(1);
   });
 
   it("should render with correct class", () => {
-    const component = shallow(<Input className="test" />);
+    const component = setup({ className: "test" });
     const wrapper = component.find(".test");
     expect(wrapper.length).toBe(1);
   });
 
   it("should render with correct type", () => {
-    const component = shallow(<Input type="submit" />);
+    const component = setup({ type: "submit" });
     const wrapper = component.find(".input");
     expect(wrapper.props().type).toBe("submit");
   });
 
   it("should handle onChange event", () => {
     const onChange = jest.fn();
-    const component = shallow(<Input onChange={onChange} />);
+    const component = setup({ onChange });
     const wrapper = component.find(".input");
     wrapper.simulate("change", { target: { value: "" } });
     expect(onChange).toHaveBeenCalled();
