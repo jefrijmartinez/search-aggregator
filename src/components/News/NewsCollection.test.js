4ebdf1932fc9ce4ea28d6318f237fda069781f83
Jefri Martinez <jefrijmartinez@gmail.com>
2022-01-27 01:33:41 -0400

    NewsCollection tests update

--- src/components/News/NewsCollection.test.js
+++ src/components/News/NewsCollection.test.js
@@ -1,9 +1,18 @@
 import NewsCollection from "./NewsCollection";
 import { shallow } from "enzyme";
 
+const setup = (props = {}) => {
+  const defaultProps = {
+    news: [],
+  };
+  const propsPassed = { ...defaultProps, ...props };
+  const wrapper = shallow(<NewsCollection {...propsPassed} />);
+  return wrapper;
+};
+
 describe("NewsCollection", () => {
   it("should render without errors", () => {
-    const component = shallow(<NewsCollection news={[]} />);
+    const component = setup();
     const wrapper = component.find(".news-collection");
     expect(wrapper.length).toBe(1);
   });
