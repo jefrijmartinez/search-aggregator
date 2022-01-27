6ba0f4639b4304fb9175f7a49ecc0bff334952a0
Jefri Martinez <jefrijmartinez@gmail.com>
2022-01-27 01:34:48 -0400

    Updating dropdown tests

--- src/components/UI/Dropdown/Dropdown.test.js
+++ src/components/UI/Dropdown/Dropdown.test.js
@@ -1,38 +1,58 @@
 import Dropdown from "./Dropdown";
 import { shallow } from "enzyme";
 
-describe("Dropdown", () => {
-  it("should render without errors", () => {
-    const options = [
+const mockSetSelectedValue = jest.fn();
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: (initialState) => [initialState, mockSetSelectedValue],
+}));
+
+const setup = (props = {}) => {
+  const defaultProps = {
+    options: [
       { value: "one", text: "One" },
       { value: "two", text: "Two" },
-    ];
-    const component = shallow(<Dropdown options={options} />);
+    ],
+  };
+  const propsPassed = { ...defaultProps, ...props };
+  const wrapper = shallow(<Dropdown {...propsPassed} />);
+  return wrapper;
+};
+
+describe("Dropdown", () => {
+  it("should render without errors", () => {
+    const component = setup();
     const wrapper = component.find(".dropdown");
     expect(wrapper.length).toBe(1);
   });
 
   it("should render with correct class", () => {
-    const options = [
-      { value: "one", text: "One" },
-      { value: "two", text: "Two" },
-    ];
-    const component = shallow(<Dropdown className="test" options={options} />);
+    const component = setup({ className: "test" });
     const wrapper = component.find(".test");
     expect(wrapper.length).toBe(1);
   });
 
   it("should render with correct onChange event", () => {
-    const options = [
-      { value: "one", text: "One" },
-      { value: "two", text: "Two" },
-    ];
     const onChange = jest.fn();
-    const component = shallow(
-      <Dropdown options={options} onChange={onChange} />
-    );
+    const component = setup({ onChange });
     const wrapper = component.find(".dropdown");
     wrapper.simulate("change", { target: { value: "one" } });
     expect(onChange).toHaveBeenCalled();
+  });
+
+  it("should call setSelectedValue when option is selected", () => {
+    const onChange = jest.fn();
+    const component = setup({ onChange });
+    const wrapper = component.find(".dropdown");
+    wrapper.simulate("change", { target: { value: "one" } });
+    expect(mockSetSelectedValue).toHaveBeenCalled();
+  });
+
+  it("should call onChange with right value when option is selected", () => {
+    const onChange = jest.fn();
+    const component = setup({ onChange });
+    const wrapper = component.find(".dropdown");
+    wrapper.simulate("change", { target: { value: "one" } });
+    expect(onChange).toHaveBeenCalledWith("one");
   });
 });
