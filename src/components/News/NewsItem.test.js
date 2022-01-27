e8e26fe6ea49a90f8f9025ea6d74e5104bc444b7
Jefri Martinez <jefrijmartinez@gmail.com>
2022-01-27 01:33:50 -0400

    NewsItem test update

--- src/components/News/NewsItem.test.js
+++ src/components/News/NewsItem.test.js
@@ -1,9 +1,15 @@
 import NewsItem from "./NewsItem";
 import { shallow } from "enzyme";
 
+const setup = (props = {}) => {
+  const propsPassed = { ...props };
+  const wrapper = shallow(<NewsItem {...propsPassed} />);
+  return wrapper;
+};
+
 describe("NewsItem", () => {
   it("should render without errors", () => {
-    const component = shallow(<NewsItem />);
+    const component = setup();
     const wrapper = component.find(".news-item");
     expect(wrapper.length).toBe(1);
   });
